65fe637912fd8990cf548d9aa8636fd78fdbacee
Moshood Owolabi <98819100+Msdot001@users.noreply.github.com>
2022-12-24 12:24:53 +0100

    Template inheritance

--- opon_imos/urls.py
+++ opon_imos/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = "opon_imos"
+app_name = "opon_imos" # represent the namespace in the base.html 
 urlpatterns = [
     # Home page
     path("", views.homepage, name="home"),
