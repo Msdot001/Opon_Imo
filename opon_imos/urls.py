30fd43dc806637b566aba13120389fb89643e84b
Moshood Owolabi <98819100+Msdot001@users.noreply.github.com>
2022-12-26 15:17:56 +0100

    Create entries for individual topic

--- opon_imos/urls.py
+++ opon_imos/urls.py
@@ -5,6 +5,10 @@ app_name = "opon_imos" # represent the namespace in the base.html
 urlpatterns = [
     # Home page
     path("", views.homepage, name="home"),
+    # Detail page for all topics.
     path("topics/", views.topics, name="topics"),
+    # Detail page for a single topic.
+    path("topics/<int:topic_id>/", views.topic, name="topic"),
+
 ]
  
--- opon_imos/views.py
+++ opon_imos/views.py
@@ -6,8 +6,20 @@ def homepage(request):
     """The home page for Opon imo"""
     return render(request, "home.html")
 
+
 def topics(request):
     """Show all topics."""
     topics = Topic.objects.order_by("date_added")
-    context = {"topics" : topics}
+    context = {"topics": topics}
     return render(request, "topics.html", context)
+
+
+def topic(request, topic_id):
+    """Show a single topic and all its entries."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by(
+        "-date_added"
+    )  # To get data through a foreign key relationship, you use the lowercase name of the related model followed by an underscore and the word set
+    context = {"topic": topic, "entries": entries}
+    return render(request, "topic.html", context)
+
