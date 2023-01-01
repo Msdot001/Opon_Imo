a51983e3c77a9f74763908dc29b1bb7983db15fa
Moshood Owolabi <98819100+Msdot001@users.noreply.github.com>
2023-01-01 11:06:22 +0100

    Create register_user

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include 
+from . import views
 
 app_name = "users"
 
 urlpatterns = [ 
-    # Include default auth urls.
-    path("", include('django.contrib.auth.urls'))
+    # Include default auth urls. for login and logout 
+    path("", include('django.contrib.auth.urls')),
+    # path for registration of new users
+    path("register/", views.register, name="register"),
 ]
 
 
