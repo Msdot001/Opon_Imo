62e0b24f0a60bd1cb75bf7ac32e0c5d82d920ba3
Moshood Owolabi <98819100+Msdot001@users.noreply.github.com>
2023-01-01 10:12:20 +0100

    Create User authentication

--- opon_imo/settings.py
+++ opon_imo/settings.py
@@ -27,7 +27,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     #local
-    "opon_imos"
+    "opon_imos",
+    "users",
+
 ]
 
 MIDDLEWARE = [
--- opon_imo/urls.py
+++ opon_imo/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("opon_imos.urls")),
+    path("users/", include("users.urls")),
 ]
